8c28071ce19aa9c0265cb62081ba04d0483c639a
YvanPidial <129046197+YvanPidial@users.noreply.github.com>
2024-05-14 10:26:35 +0400

    Update sgAMITest.py

--- sgAMITest.py
+++ sgAMITest.py
@@ -1,5 +1,5 @@
 def main():
-    print("This is a custom action triggered by an Action Menu Item in ShotGrid.")
+    print("This is a custom action triggered by an Action Menu Item in ShotGrid by Yvan.")
 
 if __name__ == "__main__":
     main()
